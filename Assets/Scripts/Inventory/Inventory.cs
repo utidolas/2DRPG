3494a170e928f82c5c3c754c18be45861e9c3682
Caiua Utida <utidapro@gmail.com>
2025-06-07 21:53:06 +0900

    inventory UI and Stack logic

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,92 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : Singleton<Inventory>
+{
+    [Header("Config")]
+    [SerializeField] private int inventorySize;
+    [SerializeField] private InventoryItem[] inventoryItems; // items to be saved inside inventory
+
+    [Header("Testing")]
+    public InventoryItem testItem;
+
+    public int InventorySize => inventorySize; // prop to get inventorySize value
+
+    public void Start()
+    {
+        inventoryItems = new InventoryItem[inventorySize];
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            AddItem(testItem, 9);
+        }
+    }
+
+    // add item to inv
+    public void AddItem(InventoryItem item, int quantity)
+    {
+        if (item == null || quantity <= 0) return;
+        List<int> itemIndexes = CheckItemStock(item.ID);
+        if(item.IsStackable && itemIndexes.Count > 0) // if we have same item in inv
+        {
+            foreach(int index in itemIndexes)
+            {
+                int maxStack = item.MaxStack;
+                if (inventoryItems[index].Quantity < maxStack) 
+                {
+                    inventoryItems[index].Quantity += quantity;
+                    if (inventoryItems[index].Quantity > maxStack)
+                    {
+                        int dif = inventoryItems[index].Quantity - maxStack;
+                        inventoryItems[index].Quantity = maxStack;
+                        AddItem(item, dif);
+                    }
+
+                    InventoryUI.Instance.DrawItem(inventoryItems[index], index);
+                    return;
+                }
+            }
+        }
+
+        int quantityToAdd = quantity > item.MaxStack ? item.MaxStack : quantity; // if quantity IS greater than itemMaxStack, quantityToAdd = item.MaxStack, otherwise quantityToAdd = quantity
+        AddItemFreeSlot(item, quantityToAdd);
+        int remainingAmount = quantity - quantityToAdd;
+        if(remainingAmount > 0)
+        {
+            AddItem(item, remainingAmount);
+        }
+    }
+
+    // add to free slot
+    private void AddItemFreeSlot(InventoryItem item, int quantity)
+    {
+        for (int i = 0; i < inventorySize; i++)
+        {
+            if (inventoryItems[i] != null) continue;
+            inventoryItems[i] = item.CopyItem();
+            inventoryItems[i].Quantity = quantity;
+            InventoryUI.Instance.DrawItem(inventoryItems[i], i);
+            return;
+        }
+    }
+
+    private List<int> CheckItemStock(string itemID)
+    {
+        // check if have the same item in inv
+        List<int> itemIndexes = new List<int>();
+        for(int i = 0; i < inventoryItems.Length; i++)
+        {
+            if (inventoryItems[i] == null) continue;
+            if (inventoryItems[i].ID == itemID) // found item in inv
+            {
+                itemIndexes.Add(i);
+            }
+        }
+
+        return itemIndexes;
+    }
+}
---
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    [Header("Config")]
+    [SerializeField] private Image itemIcon;
+    [SerializeField] private Image quantityContainer;
+    [SerializeField] private TextMeshProUGUI itemQuantityTMP;
+
+    public int Index { get; set; }
+
+    public void UpdateSlot(InventoryItem item)
+    {
+        itemIcon.sprite = item.Icon;
+        itemQuantityTMP.text = item.Quantity.ToString();
+    }
+
+    public void ShowSlotInformation(bool value)
+    {
+        itemIcon.gameObject.SetActive(value);
+        quantityContainer.gameObject.SetActive(value);
+    }
+}
